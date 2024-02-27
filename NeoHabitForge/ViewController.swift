b9f5e7562b69ce0e2c7df4e02cfb3c1dc8b3f5a6
dias-btt <dias.saiynov@nu.edu.kz>
2024-02-27 17:03:27 +0600

    built project

---
+++ NeoHabitForge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeoHabitForge
+//
+//  Created by Диас Сайынов on 27.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
